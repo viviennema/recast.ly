864268592259c708ca6e727f749aad99877c6a4a
Nick Cassano <romanlaughs@gmail.com>
2022-07-11 20:14:25 -0700

    Complete rendering App component to the DOM

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,8 @@ var App = () => (
   </div>
 );
 
+console.log(App);
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
+ReactDOM.render(<App />, document.getElementById('app'));
 export default App;
