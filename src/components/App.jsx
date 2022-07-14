5b0b51e91ca5f83713d0594721fe8b384de40b40
Nick Cassano <romanlaughs@gmail.com>
2022-07-13 19:19:08 -0700

    Complete creating a dynamic Video Player component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import videosArray from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
-
+import VideoPlayer from './VideoPlayer.js';
 
 var App = (props) => (
   <div>
@@ -11,7 +11,7 @@ var App = (props) => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <VideoPlayer video={videosArray[0]}/>
       </div>
       <div className="col-md-5">
         <VideoList videos={videosArray}/>
